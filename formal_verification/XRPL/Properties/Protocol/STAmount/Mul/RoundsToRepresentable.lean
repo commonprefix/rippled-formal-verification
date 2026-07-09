1c4bf83c24de53f6b00b6cec62bfe584592d97ff
Orfeas Stefanos Thyfronitis Litos <orfeas.litos@proton.me>
2026-07-09 10:19:26 +0000

    Add comment on rounding error denominations

--- formal_verification/XRPL/Properties/Protocol/STAmount/Mul/RoundsToRepresentable.lean
+++ formal_verification/XRPL/Properties/Protocol/STAmount/Mul/RoundsToRepresentable.lean
@@ -1,3 +1,5 @@
+-- Multiplication rounding errors, denominated in computed-value ULP increments
+
 import XRPL.Properties.Protocol.STAmount.Mul.Common.Integral
 import XRPL.Properties.Protocol.STAmount.Mul.Common.RoundsToRepresentableProofs
 
